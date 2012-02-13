9658a6a190790add95a3b4d11d7ffa9dc0e512a8
Daniel Nishimura <dnishimura@gmail.com>
2012-02-12 14:11:31 -1000

    Fix tests

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 
 describe Matrix do
   before(:all) do
-    @matrix = Matrix.build(3, 4) {0}
+    @matrix = Matrix.build(3, 4) {|r,c| r*4 + c}
   end
 
   it "should flatten" do
-    @matrix = Matrix.build(3, 4) {0}
     @matrix.respond_to?(:flatten).should be_true
     matrix_flat = @matrix.flatten
     matrix_flat.size.should eq(12)
-    matrix_flat.should eq([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    matrix_flat.should eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
   end
 
   it "should have column_size" do
